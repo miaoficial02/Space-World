a884fc854441d6d9a822984d8c6fe2210c479fae
𝑬𝒓𝒆𝒏𝒙𝒛𝒚 <diegolesimarcelino@gmail.com>
2025-07-19 17:35:04 -0400

    Actualizar jadibot-bots.js

--- plugins/jadibot-bots.js
+++ plugins/jadibot-bots.js
@@ -11,19 +11,19 @@ async function handler(m, { conn: _envio, usedPrefix }) {
   })
 
   const message = Array.from(uniqueUsers.values()).map((user, index) => `
-╭───♡⃛ BOT #${index + 1}
-│ 🌷 Usuario: @${user.jid.replace(/[^0-9]/g, '')}
-│ 💌 Link: wa.me/${user.jid.replace(/[^0-9]/g, '')}
-│ 🧁 Nombre: ${user.name || 'Roxy-MD 💖'}
+╭───➤ BOT #${index + 1}
+│ ✦ 𝙐𝙨𝙪𝙖𝙧𝙞𝙤: @${user.jid.replace(/[^0-9]/g, '')}
+│ ✦ 𝙇𝙞𝙣𝙠: wa.me/${user.jid.replace(/[^0-9]/g, '')}
+│ ✦ 𝙉𝙤𝙢𝙗𝙧𝙚: ${user.name || 'ᰔᩚ𝙁𝙚𝙣𝙧𝙮𝙎𝙪𝙗𝘽𝙤𝙩ᰔᩚ'}
 ╰───────────────`).join('\n')
 
   const replyMessage = message.length === 0
     ? '🌸 No hay bots activos en este momento...'
     : message
 
-  const responseMessage = `🎀 𓂃 ʀᴏxʏ-ᴍᴅ 𝒋𝒂𝒅𝒊𝒃𝒐𝒕𝒔 𝒂𝒄𝒕𝒊𝒗𝒐𝒔 💖\n\n${replyMessage}`
+  const responseMessage = `🎀 𝑭𝒆𝒏𝒓𝒚𝒔 𝑱𝒂𝒅𝒊𝑩𝒐𝒕 𝑨𝒄𝒕𝒊𝒗𝒐𝒔 🤍\n\n${replyMessage}`
 
-  let img = await (await fetch(`https://files.catbox.moe/nhaqir.jpg`)).buffer()
+  let img = await (await fetch(`https://files.catbox.moe/q94jaz.jpg`)).buffer()
 
   await _envio.sendFile(m.chat, img, 'roxy-jadibots.jpg', responseMessage, m, false, {
     mentions: _envio.parseMention(responseMessage)
