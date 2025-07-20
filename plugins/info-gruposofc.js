f03e83245ad0963f922296580cc9ad89c2debcf7
𝑬𝒓𝒆𝒏𝒙𝒛𝒚 <diegolesimarcelino@gmail.com>
2025-07-19 20:37:03 -0400

    Actualizar info-gruposofc.js

--- plugins/info-gruposofc.js
+++ plugins/info-gruposofc.js
@@ -1,29 +1,31 @@
 const handler = async (m, { conn, usedPrefix, command }) => {
   const texto = `
-🌐 *Grupos Oficiales de FenrysBot* 🚀
+🌐 𝙂𝙧𝙪𝙥𝙤𝙨 𝙊𝙛𝙞𝙘𝙞𝙖𝙡 𝘿𝙚 𝙁𝙚𝙣𝙧𝙮𝙨𝘽𝙤𝙩 🚀
 
 ✨ Únete a nuestra comunidad, comparte ideas, reporta errores, o simplemente charla con otros usuarios. ¡Eres bienvenido!
 
-🤍 *Lista de grupos:*
-1️⃣  *Grupo Oficial💮✨*  
+📂 𝙇𝙞𝙨𝙩𝙖 𝘿𝙚 𝙂𝙧𝙪𝙥𝙤𝙨 𝘿𝙤𝙣𝙙𝙚 𝙀𝙨𝙩𝙖 𝙁𝙚𝙣𝙧𝙮𝙨 𝘽𝙤𝙩
+1️⃣  𝙂𝙧𝙪𝙥𝙤 𝙊𝙛𝙞𝙘𝙞𝙖𝙡 𝘿𝙚 𝙁𝙚𝙣𝙧𝙮𝙨 (𝘾𝙚𝙧𝙤 𝙎𝙪𝙗𝘽𝙤𝙩)  
 https://chat.whatsapp.com/LTqtyYVc91oKdM4N0hZdko?mode=r_c
 
-2️⃣  *Grupo Solo Sub-bot🤖✨*  
+2️⃣  𝙂𝙧𝙪𝙥𝙤 𝙊𝙛𝙞𝙘𝙞𝙖𝙡 𝙎𝙪𝙗𝘽𝙤𝙩  
 https://chat.whatsapp.com/FkBNSxVurdR2dX6m81qaZI?mode=r_c
 
-3️⃣  *Grupo De Coloboracion🌸✨*  
+3️⃣  𝙂𝙧𝙪𝙥𝙤 𝙊𝙛𝙞𝙘𝙞𝙖𝙡 𝘾𝙤𝙡𝙤𝙗𝙤𝙧𝙖𝙘𝙞𝙤𝙣 𝙒𝙖𝙜𝙪𝙧𝙞 𝙄𝘼 𝙭 𝙁𝙚𝙣𝙧𝙮𝙨𝘽𝙤𝙩  
 https://chat.whatsapp.com/Fqks1pKKlRLH9UbelmeDck?mode=r_c
 
 ⚠️ Respeta las normas de cada grupo.
 
 ─
-> Usa *.menu* para ver todos los comandos.
+📌 Usa *.menu* para ver todos los comandos.
+`
+
   await conn.sendMessage(m.chat, {
     text: texto.trim(),
     contextInfo: {
       externalAdReply: {
-        title: "FenrysBot 🌸✨",
-        body: "Únete a nuestros grupos oficiales",
+        title: "𝑭𝒆𝒏𝒓𝒚𝒔𝑩𝒐𝒕-𝑴𝑫",
+        body: "*Únete a nuestros grupos oficiales*",
         thumbnailUrl: 'https://files.catbox.moe/j6ci3o.jpg', // Puedes cambiar la imagen
         sourceUrl: "https://github.com/El-brayan502/NyanCatBot-MD",
         mediaType: 1,
