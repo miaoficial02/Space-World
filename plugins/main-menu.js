5b148ee031bf5c9ce929dd010134dd26918226ac
𝑬𝒓𝒆𝒏𝒙𝒛𝒚 <diegolesimarcelino@gmail.com>
2025-07-19 23:54:26 -0400

    Actualizar main-menu.js

--- plugins/main-menu.js
+++ plugins/main-menu.js
@@ -24,9 +24,9 @@ let handler = async (m, { conn, usedPrefix, __dirname, participants }) => {
     let pais = phone.getRegionCode() || 'Desconocido 🌐'
 
     const vids = [
-      'https://files.catbox.moe/w5t38u.mp4',
-      'https://files.catbox.moe/w5t38u.mp4',
-      'https://files.catbox.moe/w5t38u.mp4'
+      'https://files.catbox.moe/3h35sn.mp4',
+      'https://files.catbox.moe/3h35sn.mp4',
+      'https://files.catbox.moe/3h35sn.mp4'
     ]
     let videoUrl = vids[Math.floor(Math.random() * vids.length)]
 
