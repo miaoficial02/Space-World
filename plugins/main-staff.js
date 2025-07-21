3a5f95abee240ddb0a57fe82b28a3ae1a6741171
𝑬𝒓𝒆𝒏𝒙𝒛𝒚 <diegolesimarcelino@gmail.com>
2025-07-21 14:57:51 -0400

    Actualizar main-staff.js

--- plugins/main-staff.js
+++ plugins/main-staff.js
@@ -1,38 +1,38 @@
 let handler = async (m, { conn, command, usedPrefix }) => {
-let staff = `✨ *EQUIPO DE AYUDANTES*
+let staff = `𝑷𝑹𝑶𝒀𝑬𝑪𝑻𝑶 𝑭𝑬𝑵𝑹𝒀𝑺-𝑺𝑻𝑨𝑭𝑭 🌟
 🤖 *Bot:* ${global.botname}
-🌟 *Versión:* ${global.vs}
+🔰 *Versión:* 1.0
 
-👑 *Propietario:*
+👑 𝑷𝑹𝑶𝑷𝑰𝑬𝑻𝑨𝑹𝑰𝑶𝑺 𝑫𝑬𝑳 𝑩𝑶𝑻𝑺 👑
 
-• 🐉𝙉𝙚𝙤𝙏𝙤𝙠𝙮𝙤 𝘽𝙚𝙖𝙩𝙨🐲
-🤴 *Rol:* Propietario
-📱 *Número:* wa.me/50248019799
-✨️ *GitHub:* https://github.com/TOKIO5025
+• 𝑬𝑹𝑬𝑵𝑿𝑺𝒁𝒀-𝑴𝑫𝑭 
+✦ *Rol:* 𝑷𝒓𝒐𝒑𝒊𝒆𝒕𝒂𝒓𝒊𝒐𝒔 𝑫𝒆 𝑭𝒆𝒏𝒓𝒚𝒔𝑩𝒐𝒕 
+➤ *Número:* wa.me/18493907272
+✦ *GitHub:* https://github.com/TOKIO5025
 
-🚀  *Colaboradores:*
+🤍  *Colaboradores:*
 
-• Emer
-🦁 *Rol:* Developer
-📱 *Número:* Wa.me/51984368849
+• 𝑬𝑳 𝑩𝑹𝒀𝑨𝑵 
+✦ *Rol:* 𝑪𝒐𝒍𝒐𝒃𝒐𝒓𝒂𝒅𝒐𝒓 𝒙 𝑨𝒚𝒖𝒅𝒂𝒏𝒕𝒆 
+➤ *Número:* Wa.me/573001533523
 
-• Dioneibi
-🐯 *Rol:* Contribuidor
-📱 *Número:* Wa.me/18294868853
+• 𝑪𝑯𝑶𝑳𝑰𝑻𝑶-𝑿𝒁𝒀
+✦ *Rol:* 𝑪𝒐𝒍𝒐𝒃𝒐𝒓𝒂𝒅𝒐𝒓 𝒙 𝑨𝒚𝒖𝒅𝒂𝒏𝒕𝒆 
+➤ *Número:* Wa.me/50489513153
 
-• 🐉𝙉𝙚𝙤𝙏𝙤𝙠𝙮𝙤 𝘽𝙚𝙖𝙩𝙨🐲
-💻 *Rol:* Soporte 
-📱 *Número:* Wa.me/50248019799
+• .....
+✦ *Rol:* Soporte 
+➤ *Número:* Wa.me/18493907272
 `
-await conn.sendFile(m.chat, icons, 'yaemori.jpg', staff.trim(), fkontak, true, {
+await conn.sendFile(m.chat, icons, 'https://files.catbox.moe/90u9mx.jpg', staff.trim(), fkontak, true, {
 contextInfo: {
 'forwardingScore': 200,
 'isForwarded': false,
 externalAdReply: {
 showAdAttribution: true,
 renderLargerThumbnail: false,
-title: `🥷 Developers 👑`,
-body: `✨ Staff Oficial`,
+title: `🌸𝑷𝑹𝑶𝒀𝑬𝑪𝑻𝑶 𝑭𝑬𝑵𝑹𝒀𝑺-𝑩𝑶𝑻🌸`,
+body: `👑𝑺𝑻𝑨𝑭𝑭-𝑶𝑭𝑰𝑪𝑰𝑨𝑳👑`,
 mediaType: 1,
 sourceUrl: redes,
 thumbnailUrl: icono
