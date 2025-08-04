72980f7423bdff52abdb725a85d37f2ccc50192c
Its.mia.oficial <hibqbqxdd@gmail.com>
2025-08-03 22:04:01 -0300

    Create grupo-setname.js

---
+++ plugins/grupo-setname.js
@@ -0,0 +1,13 @@
+var handler = async (m, { conn, text }) => {
+  if (!text) return m.reply('✏️ Escribe el nuevo nombre del grupo.');
+  await conn.groupUpdateSubject(m.chat, text);
+  m.reply('✅ Nombre del grupo actualizado.');
+};
+handler.help = ['setname'];
+handler.tags = ['grupo'];
+handler.command = ['groupname', 'setnombre', 'cambiarnombre'];
+handler.admin = true;
+handler.botAdmin = true;
+handler.group = true;
+
+export default handler;
