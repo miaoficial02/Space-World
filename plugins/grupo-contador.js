632154371388b8e7e94e1fbc642d1cb07b5dbb0a
Its.mia.oficial <hibqbqxdd@gmail.com>
2025-08-03 22:20:22 -0300

    Update grupo-contador.js

--- plugins/grupo-contador.js
+++ plugins/grupo-contador.js
@@ -1,3 +1,5 @@
+//Código creado por Mía, Github: https://github.com/miaoficial02
+
 var handler = async (m, { participants }) => {
   const total = participants.length;
   const admins = participants.filter(p => p.admin).length;
