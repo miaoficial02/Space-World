3094420f4526c9061d31223845130892ff8e16fd
Its.mia.oficial <hibqbqxdd@gmail.com>
2025-08-03 21:57:04 -0300

    Create grupo-link.js

---
+++ plugins/grupo-link.js
@@ -0,0 +1,13 @@
+var handler = async (m, { conn }) => {
+  const link = await conn.groupInviteCode(m.chat);
+  m.reply(`✿︎ Enlace del grupo:\nhttps://chat.whatsapp.com/${link}`);
+};
+
+handler.help = ['link'];
+handler.tags = ['grupo'];
+handler.command = ['link', 'linkgp', 'grupolink'];
+handler.group = true;
+handler.admin = true;
+handler.botAdmin = true;
+
+export default handler;
