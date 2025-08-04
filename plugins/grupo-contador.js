26236dfd25b847d4c447dc9b556b26c79c12529a
Its.mia.oficial <hibqbqxdd@gmail.com>
2025-08-03 22:05:27 -0300

    Create grupo-contador.js

---
+++ plugins/grupo-contador.js
@@ -0,0 +1,13 @@
+var handler = async (m, { participants }) => {
+  const total = participants.length;
+  const admins = participants.filter(p => p.admin).length;
+  const normales = total - admins;
+
+  m.reply(`👥 *Miembros del grupo:*\n- Total: ${total}\n- Admins: ${admins}\n- Miembros normales: ${normales}`);
+};
+handler.help = ['contar'];
+handler.tags = ['grupo'];
+handler.command = ['contar', 'miembros'];
+handler.group = true;
+
+export default handler;
