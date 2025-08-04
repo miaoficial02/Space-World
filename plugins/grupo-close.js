a8c53a311eb0acb4c136a4820dc52c81e15c0530
Its.mia.oficial <hibqbqxdd@gmail.com>
2025-08-03 21:59:20 -0300

    Create grupo-close.js

---
+++ plugins/grupo-close.js
@@ -0,0 +1,12 @@
+var handler = async (m, { conn }) => {
+  await conn.groupSettingUpdate(m.chat, 'announcement');
+  m.reply('🔒 Grupo cerrado. Solo los admins pueden escribir.');
+};
+handler.help = ['closegc'];
+handler.tags = ['grupo'];
+handler.command = ['closegc', 'cerrargrupo', 'close'];
+handler.admin = true;
+handler.botAdmin = true;
+handler.group = true;
+
+export default handler;
