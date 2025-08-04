6770b99fd686c80e6986526b109db485bf2c0c5a
Its.mia.oficial <hibqbqxdd@gmail.com>
2025-08-03 22:15:26 -0300

    Update grupo-setdesc.js

--- plugins/grupo-setdesc.js
+++ plugins/grupo-setdesc.js
@@ -1,3 +1,5 @@
+//Código creado por Mía Github: https://github.com/miaoficial02
+
 var handler = async (m, { conn, text }) => {
   if (!text) return m.reply('✏️ Escribe la nueva descripción del grupo.');
   await conn.groupUpdateDescription(m.chat, text);
