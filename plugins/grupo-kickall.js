70e16e35cb5cf38cdbcd0abc622121c87846867a
Its.mia.oficial <hibqbqxdd@gmail.com>
2025-08-03 22:18:16 -0300

    Update grupo-kickall.js

--- plugins/grupo-kickall.js
+++ plugins/grupo-kickall.js
@@ -1,3 +1,5 @@
+//Código creado por Mía, Github: https://github.com/miaoficial02
+
 var handler = async (m, { conn, participants }) => {
   const groupInfo = await conn.groupMetadata(m.chat);
   const ownerGroup = groupInfo.owner || m.chat.split`-`[0] + '@s.whatsapp.net';
