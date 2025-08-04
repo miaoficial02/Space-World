44cf163e6ee42d2ffc127c7d5878cb46ad05a836
Its.mia.oficial <hibqbqxdd@gmail.com>
2025-08-03 22:04:37 -0300

    Create grupo-setdesc.js

---
+++ plugins/grupo-setdesc.js
@@ -0,0 +1,13 @@
+var handler = async (m, { conn, text }) => {
+  if (!text) return m.reply('✏️ Escribe la nueva descripción del grupo.');
+  await conn.groupUpdateDescription(m.chat, text);
+  m.reply('✅ Descripción del grupo actualizada.');
+};
+handler.help = ['setdesc'];
+handler.tags = ['grupo'];
+handler.command = ['groupdesc', 'descripcion', 'cambiardesc'];
+handler.admin = true;
+handler.botAdmin = true;
+handler.group = true;
+
+export default handler;
